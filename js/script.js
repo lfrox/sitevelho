a370804568619aad237126656d3e6e1f501fb208
lfrox <luizfelipergs88@gmail.com>
2018-06-19 17:09:10 -0300

    alterações dia 19

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 var menu = document.querySelector(".menu")
+var setacima = document.querySelector(".setacima")
+var setabaixo = document.querySelector(".setabaixo")
 
 menu.addEventListener("click", () => {
   menu.classList.toggle("hide")
   menu.classList.toggle("show")
+  setabaixo.classList.toggle("hide")
+  setacima.classList.toggle("show")
+  setabaixo.classList.toggle("show")
+  setacima.classList.toggle("hide")
 });
 
+
+// $(".menu").click(function() { // function() {} === () => {}
+//   $(this).toggleClass("hide show")
+//   $(".setacima").toggleClass("hide show")
+//   $(".setabaixo").toggleClass("hide show")
+// });
